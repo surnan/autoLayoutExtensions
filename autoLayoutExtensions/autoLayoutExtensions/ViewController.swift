711bdbeb501f1964daf3bc08170d39fabaee107b
surnan <forsuresh@yandex.com>
2018-02-01 21:01:35 -0500

    project runs successfully without storyboard.  Early stages.

---
+++ autoLayoutExtensions/autoLayoutExtensions/ViewController.swift
@@ -0,0 +1,22 @@
+//
+//  ViewController.swift
+//  autoLayoutExtensions
+//
+//  Created by admin on 2/1/18.
+//  Copyright © 2018 admin. All rights reserved.
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        view.backgroundColor = .yellow
+        
+     
+    
+    
+    }
+}
+
